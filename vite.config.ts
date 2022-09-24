8dc0ba86364a28b2b354fcbeb48fb2de81842b6e
youkonwHRT <1021300769@qq.com>
2022-09-24 13:37:13 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/mangosteen/dist/',
+  // base:'/mangosteen/dist/',
   plugins: [vue()]
 })
