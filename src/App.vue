77540b1fa48d76ca6059b6848324993cfcd11541
youkonwHRT <1021300769@qq.com>
2022-09-26 06:54:00 +0800

    文件创建

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    hi
+    {{count}}
+  </div>
+  <div>
+  <button @click="onClick">+1</button>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+const onClick =()=>{
+  count.value ++
+}
+</script>
+<style>
+
+</style>
