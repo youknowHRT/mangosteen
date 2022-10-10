1c27a42e2f354c535e7a2163f2b2324f2fde2ef7
youkonwHRT <1021300769@qq.com>
2022-10-11 06:48:37 +0800

    feat: first-welcome view

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -13,10 +13,10 @@ export const routes: RouteRecordRaw[] = [
   { path: '/welcome',
     component: Welcome,
     children:[
-      {path:'/1',component:  First},
-      {path:'/2',component: Second},
-      {path:'/3',component: Third},
-      {path:'/4',component: Forth},
+      {path:'1',component: First},
+      {path:'2',component: Second},
+      {path:'3',component: Third},
+      {path:'4',component: Forth},
     ]
   }
 ]
