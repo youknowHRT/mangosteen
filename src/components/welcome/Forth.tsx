08403158cf643d77ae4c0acd5d48e56a9deb5da1
youknowHRT <1021300769@qq.com>
2022-10-26 23:57:54 +0800

    feat: 提取WelcomeLayout组件

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,9 +1,21 @@
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import s from './WelcomeLayout.module.scss'
+import cloud from '../../assets/icons/cloud.svg'
+import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Forth = defineComponent({
   setup(props, ctx) {
-    return ()=>(
-      <div> Forth </div>
-    )
+    return <WelcomeLayout>
+      {{
+        icon:()=><img src={cloud} />,
+        title:()=><h2>云备份<br/>再也不怕数据丢失</h2>,
+        buttons:()=><>
+           <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+           <RouterLink to='/start'>完成</RouterLink>
+           <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+        </>
+      }}
+    </WelcomeLayout>
   }
 })
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,9 +1,23 @@
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import s from './WelcomeLayout.module.scss'
+import chart from '../../assets/icons/chart.svg'
+import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Third = defineComponent({
   setup(props, ctx) {
     return ()=>(
-      <div> Third </div>
+      <WelcomeLayout>
+        {{
+          icon: ()=><img src={chart} />,
+          title:()=><h2>数据可视化<br/>收支一目了然</h2>,
+          buttons: ()=><>
+            <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+            <RouterLink to='/welcome/4'>下一页</RouterLink>
+            <RouterLink to='/start'>跳过</RouterLink>
+          </>
+        }}
+      </WelcomeLayout>
     )
   }
 })
