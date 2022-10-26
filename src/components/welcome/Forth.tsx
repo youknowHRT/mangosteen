07baf99987036cfa4425471b1197220211a2b91d
youknowHRT <1021300769@qq.com>
2022-10-27 00:18:57 +0800

    .

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -13,7 +13,7 @@ export const Forth = defineComponent({
         buttons:()=><>
            <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
            <RouterLink to='/start'>完成</RouterLink>
-           <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+           <RouterLink class={s.fake} to='/start'>跳过s</RouterLink>
         </>
       }}
     </WelcomeLayout>
