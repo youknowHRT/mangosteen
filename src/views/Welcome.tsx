d0a80753b7a8fa74b80d8d5cf1c66fbc07610261
youknowHRT <1021300769@qq.com>
2022-11-23 23:14:59 +0800

    feat: 避免后退

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -15,13 +15,13 @@ export const Welcome = defineComponent({
     const {direction, swiping}= useSwipe(main,{ beforeStart: (e) => e.preventDefault() })
     const router = useRouter()
     const route = useRoute()
-    const push=throttle(()=>{
+    const replace=throttle(()=>{
       const name = (route.name || 'Welcome1').toString()
-      router.push(pushMap[name])
+      router.replace(pushMap[name])
     },500)
     watchEffect(()=>{
       if(swiping.value && direction.value ==='left'){
-        push()
+        replace()
       }
     })
     return ()=><div class={s.wrapper}>
