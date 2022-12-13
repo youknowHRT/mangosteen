0052eee7aa1d493c86121f9e55ec71af0f1483c4
youknowHRT <1021300769@qq.com>
2022-12-14 00:23:21 +0800

    wip: start page

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,13 +1,17 @@
 import { defineComponent, defineProps, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' 
+// | 'charts' | 'notify' | 'export'
 
 export const Icon = defineComponent({
   props: {
     name: {
       type: String as PropType<IconName>,
       required: true,
+    },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
     }
   },
   setup: (props, context) => {
