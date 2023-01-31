d80abdc44563575a717326a868bebe44ffc67708
youknowHRT <1021300769@qq.com>
2023-02-01 00:24:47 +0800

    feat: tag Edit页面制作

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,14 +1,23 @@
-import { defineComponent } from 'vue';
-import s from './Button.module.scss'
+import { defineComponent, PropType } from 'vue';
+import s from './Button.module.scss';
 
-interface Props{
-  onClick?: (e: MouseEvent) => void
+interface Props {
 }
-export const Button = defineComponent<Props>({
-  setup(props, ctx) {
-    return ()=>(
-      <button class={s.button}> 
-        {ctx.slots.default?.()}
+
+export const Button = defineComponent({
+  props: {
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
+    },
+    level: {
+      type: String as PropType<'important' | 'normal' | 'danger'>,
+      default: 'important'
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <button class={[s.button, s[props.level]]}>
+        {context.slots.default?.()}
       </button>
     )
   }
