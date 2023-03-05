49fd3724028c3f731be7d2fe50eca5fac2266d99
youknowHRT <1021300769@qq.com>
2023-03-05 23:14:36 +0800

    feat: 路由检测

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -11,6 +11,7 @@ import { Second } from "../components/welcome/Second";
 import { SecondActions } from "../components/welcome/SecondActions";
 import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
+import { http } from "../shared/Http";
 import { ItemPage } from "../views/ItemPage";
 import { SignInPage } from "../views/SignInPage";
 import { StartPage } from "../views/StartPage";
--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,16 +1,16 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosRequestHeaders } from "axios";
 type JSONValue = string | number | null | boolean | JSONValue[] | { [key: string]: JSONValue };
 
-export class Http{
+export class Http {
   instance: AxiosInstance
-  constructor(baseURL: string){
-    this.instance = axios.create({ 
+  constructor(baseURL: string) {
+    this.instance = axios.create({
       baseURL
-   })
+    })
   }
-  //read
-  get<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig,'params'|'url'|'method'>){
-    return this.instance.request<R>({ ...config, url, params: query, method: 'get' })
+  // read
+  get<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>) {
+    return this.instance.request<R>({ ...config, url: url, params: query, method: 'get' })
   }
   // create
   post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>) {
@@ -25,17 +25,27 @@ export class Http{
     return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
   }
 }
+
 export const http = new Http('/api/v1')
 
-http.instance.interceptors.response.use(response=>{
-  console.log('response')
+http.instance.interceptors.request.use(config => {
+  const jwt = localStorage.getItem('jwt')
+  if (jwt) {
+    config.headers!.Authorization = `Bearer ${jwt}`
+  }
+  return config
+})
+
+http.instance.interceptors.response.use(response => {
   return response
 }, (error) => {
-  if(error.response){
+  if (error.response) {
     const axiosError = error as AxiosError
-    if(axiosError.response?.status === 429){
+    if (axiosError.response?.status === 429) {
       alert('你太频繁了')
     }
   }
   throw error
 })
+
+
