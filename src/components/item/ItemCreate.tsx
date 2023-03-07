d39cd9a3b3956cd6291c270c928dd13d8285e313
youknowHRT <1021300769@qq.com>
2023-03-08 00:47:29 +0800

    feat: 记账新增

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,4 +1,7 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { AxiosError } from 'axios';
+import { Dialog } from 'vant';
+import { defineComponent, onMounted, PropType, reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
@@ -11,41 +14,70 @@ import { Tags } from './Tags';
 export const ItemCreate = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
-    const refKind = ref('支出')
-    const { tags: incomeTags,
-      hasMore: hasMore2,
-      fetchTags: fetchTags2
-    } = useTags((page) => {
-      return http.get<Resources<Tag>>('/tags', {
-        kind: 'income',
-        page: page + 1,
-        _mock: 'tagIndex'
-      })
-    })
+    const formData = reactive({
+      kind: '支出',
+      tags_id: [],
+      amount: 0,
+      happen_at: new Date().toISOString(),
+    });
+    const router = useRouter();
+    const onError = (error: AxiosError<ResourceError>) => {
+      if (error.response?.status === 422) {
+        Dialog.alert({
+          title: '出错',
+          message: Object.values(error.response.data.errors).join('\n'),
+        });
+      }
+      throw error;
+    };
+    const onSubmit = async () => {
+      const xxx =await http
+        .post<Resource<Item>>('/items', formData, {
+          params: { _mock: 'itemCreate' },
+        })
+        .catch(onError);
+        console.log("🚀 ~ file: ItemCreate.tsx:39 ~ onSubmit ~ xxx:", xxx)
+
+      router.push('/items');
+    };
     return () => (
-      <MainLayout class={s.layout}>{{
-        title: () => '记一笔',
-        icon: () => <Icon name="left" class={s.navIcon} />,
-        default: () => <>
-          <div class={s.wrapper}>
-            <Tabs v-model:selected={refKind.value} class={s.tabs}>
-              <Tab name="支出">
-                <Tags kind="expenses"/>
-              </Tab>
-              <Tab name="收入">
-                <Tags kind="income"/>
-              </Tab>
-            </Tabs>
-            <div class={s.inputPad_wrapper}>
-              <InputPad />
-            </div>
-          </div>
-        </>
-      }}</MainLayout>
-    )
-  }
-})
+      <MainLayout class={s.layout}>
+        {{
+          title: () => '记一笔',
+          icon: () => <Icon name="left" class={s.navIcon} />,
+          default: () => (
+            <>
+              <div class={s.wrapper}>
+                <Tabs v-model:selected={formData.kind} class={s.tabs}>
+                  <Tab name="支出">
+                    <Tags
+                      kind="expenses"
+                      v-model:selected={formData.tags_id[0]}
+                    />
+                  </Tab>
+                  <Tab name="收入">
+                    <Tags
+                      kind="income"
+                      v-model:selected={formData.tags_id[0]}
+                    />
+                  </Tab>
+                </Tabs>
+                <div class={s.inputPad_wrapper}>
+                  <InputPad
+                    v-model:happenAt={formData.happen_at}
+                    v-model:amount={formData.amount}
+                    onSubmit={onSubmit}
+                  />
+                </div>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,12 +5,32 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale('zh_CN');
 
+export const mockItemCreate: Mock = config => {
+  return [200, {
+    resource: {
+      "id": 2264,
+      "user_id": 1312,
+      "amount": 9900,
+      "note": null,
+      "tags_id": [3508],
+      "happen_at": "2020-10-29T16:00:00.000Z",
+      "created_at": "2022-07-03T15:35:56.301Z",
+      "updated_at": "2022-07-03T15:35:56.301Z",
+      "kind": "expenses"
+    }
+  }]
+}
 export const mockSession: Mock = (config) => {
   return [200, {
     jwt: faker.random.word()
   }]
 }
 
+let id = 0
+const createId = () => {
+  id += 1
+  return id
+}
 export const mockTagIndex: Mock = (config) => {
   const { kind, page } = config.params
   const per_page = 25
@@ -19,14 +39,13 @@ export const mockTagIndex: Mock = (config) => {
     page, per_page, count
   })
   const createTag = (n = 1, attrs?: any) =>
-    Array.from({ length: n }, (v, i) => i+1).map((id) => ({
-      id,
+    Array.from({ length: n }).map(() => ({
+      id: createId(),
       name: faker.lorem.word(),
       sign: faker.internet.emoji(),
       kind: config.params.kind,
       ...attrs
     }))
-
   const createBody = (n = 1, attrs?: any) => ({
     resources: createTag(n), pager: createPaper(page)
   })
