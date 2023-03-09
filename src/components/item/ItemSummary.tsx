4a9a4f46a3296b179a7383ad11c4742e0411d9e6
youknowHRT <1021300769@qq.com>
2023-03-09 22:35:00 +0800

    feat: items自定义板块处理

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,114 +1,113 @@
-import { defineComponent, PropType } from 'vue';
-import { FloatButton } from '../../shared/FloatButton';
-import s from './ItemSummary.module.scss';
+import { defineComponent, onMounted, PropType, reactive, ref, watch } from 'vue'
+import { Button } from '../../shared/Button'
+import { Datetime } from '../../shared/Datetime'
+import { FloatButton } from '../../shared/FloatButton'
+import { http } from '../../shared/Http'
+import { Money } from '../../shared/Money'
+import s from './ItemSummary.module.scss'
 export const ItemSummary = defineComponent({
   props: {
     startDate: {
       type: String as PropType<string>,
-      required: true
+      required: false,
     },
     endDate: {
       type: String as PropType<string>,
-      required: true
-    }
+      required: false,
+    },
   },
   setup: (props, context) => {
+    const items = ref<Item[]>([])
+    const hasMore = ref(false)
+    const page = ref(0)
+    const fetchItems = async () => {
+      if(!props.startDate || !props.endDate){ return }
+      const response = await http.get<Resources<Item>>('/items', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        page: page.value + 1,
+        _mock: 'itemIndex',
+      })
+      const { resources, pager } = response.data
+      items.value?.push(...resources)
+      hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
+      page.value += 1
+    }
+    onMounted(fetchItems)
+
+    watch(()=>[props.startDate,props.endDate], ()=>{
+      items.value = []
+      hasMore.value = false
+      page.value = 0
+      fetchItems()
+    })
+
+    const itemsBalance = reactive({
+      expenses: 0, income: 0, balance: 0
+    })
+    const fetchItemsBalance =async ()=>{
+      if(!props.startDate || !props.endDate){ return }
+      const response = await http.get('/items/balance', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        page: page.value + 1,
+        _mock: 'itemIndexBalance',
+      })
+      Object.assign(itemsBalance, response.data)
+    }
+    onMounted(fetchItemsBalance)
+    watch(()=>[props.startDate,props.endDate], ()=>{
+      Object.assign(itemsBalance, {
+        expenses: 0, income: 0, balance: 0
+      })
+      fetchItemsBalance()
+    })
     return () => (
       <div class={s.wrapper}>
-        <ul class={s.total}>
-          <li><span>收入</span><span>128</span></li>
-          <li><span>支出</span><span>99</span></li>
-          <li><span>净收入</span><span>39</span></li>
-        </ul>
-        <ol class={s.list}>
-          <li>
-            <div class={s.sign}>
-              <span>X</span>
+        {items.value ? (
+          <>
+            <ul class={s.total}>
+              <li>
+                <span>收入</span>
+                <Money value={itemsBalance.income} />
+              </li>
+              <li>
+                <span>支出</span>
+                <Money value={itemsBalance.expenses} />
+              </li>
+              <li>
+                <span>净收入</span>
+                <Money value={itemsBalance.balance} />
+              </li>
+            </ul>
+            <ol class={s.list}>
+              {items.value.map((item) => (
+                <li>
+                  <div class={s.sign}>
+                    <span>{item.tags![0].sign}</span>
+                  </div>
+                  <div class={s.text}>
+                    <div class={s.tagAndAmount}>
+                      <span class={s.tag}>{item.tags![0].name}</span>
+                      <span class={s.amount}>￥<Money value={item.amount}/></span>
+                    </div>
+                    <div class={s.time}><Datetime value={item.happen_at}/></div>
+                  </div>
+                </li>
+              ))}
+            </ol>
+            <div class={s.more}>
+              {hasMore.value ?
+                <Button onClick={fetchItems}>加载更多</Button> :
+                <span>没有更多</span>
+              }
             </div>
-            <div class={s.text}>
-              <div class={s.tagAndAmount}>
-                <span class={s.tag}>旅行</span>
-                <span class={s.amount}>￥1234</span>
-              </div>
-              <div class={s.time}>
-                2000-01-01 12:39
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class={s.sign}>
-              <span>X</span>
-            </div>
-            <div class={s.text}>
-              <div class={s.tagAndAmount}>
-                <span class={s.tag}>旅行</span>
-                <span class={s.amount}>￥1234</span>
-              </div>
-              <div class={s.time}>
-                2000-01-01 12:39
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class={s.sign}>
-              <span>X</span>
-            </div>
-            <div class={s.text}>
-              <div class={s.tagAndAmount}>
-                <span class={s.tag}>旅行</span>
-                <span class={s.amount}>￥1234</span>
-              </div>
-              <div class={s.time}>
-                2000-01-01 12:39
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class={s.sign}>
-              <span>X</span>
-            </div>
-            <div class={s.text}>
-              <div class={s.tagAndAmount}>
-                <span class={s.tag}>旅行</span>
-                <span class={s.amount}>￥1234</span>
-              </div>
-              <div class={s.time}>
-                2000-01-01 12:39
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class={s.sign}>
-              <span>X</span>
-            </div>
-            <div class={s.text}>
-              <div class={s.tagAndAmount}>
-                <span class={s.tag}>旅行</span>
-                <span class={s.amount}>￥1234</span>
-              </div>
-              <div class={s.time}>
-                2000-01-01 12:39
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class={s.sign}>
-              <span>X</span>
-            </div>
-            <div class={s.text}>
-              <div class={s.tagAndAmount}>
-                <span class={s.tag}>旅行</span>
-                <span class={s.amount}>￥1234</span>
-              </div>
-              <div class={s.time}>
-                2000-01-01 12:39
-              </div>
-            </div>
-          </li>
-        </ol>
-        <div class={s.more}>向下滑动加载更多</div>
-        <FloatButton iconName='add' />
+          </>
+        ) : (
+          <div>记录为空</div>
+        )}
+        <FloatButton iconName="add" />
       </div>
     )
-  }
+  },
 })
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,6 +5,57 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale('zh_CN');
 
+export const mockItemIndexBalance: Mock = config => {
+  return [200, {
+    expenses: 9900,
+    income: 9900,
+    balance: 0
+  }]
+}
+export const mockItemIndex: Mock = (config) => {
+  const { kind, page } = config.params
+  const per_page = 25
+  const count = 26
+  const createPaper = (page = 1) => ({
+    page,
+    per_page,
+    count,
+  })
+  const createTag = (attrs?: any) =>
+    ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: 'expenses',
+      ...attrs
+    })
+  const createItem = (n = 1, attrs?: any) =>
+    Array.from({ length: n }).map(() => ({
+      id: createId(),
+      user_id: createId(),
+      amount: Math.floor(Math.random() * 10000),
+      tags_id: [createId()],
+      tags: [createTag()],
+      happen_at: faker.date.past().toISOString(),
+      kind: config.params.kind,
+    }))
+  const createBody = (n = 1, attrs?: any) => ({
+    resources: createItem(n),
+    pager: createPaper(page),
+    summary: {
+      income: 9900,
+      expenses: 9900,
+      balance: 0
+    }
+  })
+  if (!page || page === 1) {
+    return [200, createBody(25)]
+  } else if (page === 2) {
+    return [200, createBody(1)]
+  }else{
+    return [200, {}]
+  }
+}
 export const mockTagEdit: Mock = config => {
   const createTag = (attrs?: any) =>
     ({
